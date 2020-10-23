19b8f0f7543d97cac4b67f7e656ada9645d7ee92
aianlinb <ianlin0110g@gmail.com>
2020-10-23 22:56:20 +0800

    FIx replacing

--- LibGGPK2/Records/FileRecord.cs
+++ LibGGPK2/Records/FileRecord.cs
@@ -21,9 +21,8 @@ namespace LibGGPK2.Records
         /// Length of the raw file data
         /// </summary>
         public int DataLength;
-        /// <summary>
-        /// Directory this file resides in
-        /// </summary>
+        
+		public override SortedSet<RecordTreeNode> Children => null;
 
         public FileRecord(int length, GGPKContainer ggpk)
         {
@@ -63,11 +62,13 @@ namespace LibGGPK2.Records
         /// <summary>
         /// Get the file content of this record
         /// </summary>
-        public virtual byte[] ReadFileContent()
+        public virtual byte[] ReadFileContent(Stream stream = null)
         {
             var buffer = new byte[DataLength];
-            ggpkContainer.fileStream.Seek(DataBegin, SeekOrigin.Begin);
-            ggpkContainer.Reader.Read(buffer, 0, DataLength);
+            if (stream == null)
+                stream = ggpkContainer.fileStream;
+            stream.Seek(DataBegin, SeekOrigin.Begin);
+            stream.Read(buffer, 0, DataLength);
             return buffer;
         }
 
@@ -122,29 +123,19 @@ namespace LibGGPK2.Records
                 else
                 {
                     FreeRecord free = bestNode.Value;
-                    bw.BaseStream.Seek(free.RecordBegin, SeekOrigin.Begin); // Write to the FreeRecord
+                    bw.BaseStream.Seek(free.RecordBegin + free.Length - Length, SeekOrigin.Begin); // Write to the FreeRecord
                     Write();
+                    DataBegin = bw.BaseStream.Position;
                     bw.Write(NewContent);
                     free.Length = space;
                     if (space >= 16) // Update offset of FreeRecord
                     {
-                        free.Write();
-                        var PreviousBest = bestNode.Previous?.Value;
-                        if (PreviousBest == null)
-                        {
-                            ggpkContainer.ggpkRecord.FirstFreeRecordOffset = free.RecordBegin;
-                            bw.BaseStream.Seek(ggpkContainer.ggpkRecord.RecordBegin + 20, SeekOrigin.Begin);
-                            bw.Write(free.RecordBegin);
-                        }
-                        else
-                        {
-                            PreviousBest.NextFreeOffset = free.RecordBegin;
-                            bw.BaseStream.Seek(PreviousBest.RecordBegin + 8, SeekOrigin.Begin);
-                            bw.Write(free.RecordBegin);
-                        }
+                        bw.BaseStream.Seek(free.RecordBegin, SeekOrigin.Begin);
+                        bw.Write(free.Length);
                     }
                     else // Remove the FreeRecord
                         free.Remove(bestNode);
+					bw.Flush();
                 }
 
                 UpdateOffset(oldOffset); // Update the offset of FileRecord in Parent.Entries/>
@@ -217,73 +208,73 @@ namespace LibGGPK2.Records
                 {
                     switch (Path.GetExtension(Name).ToLower())
                     {
-                        case "act":
-                        case "ais":
-                        case "amd": // Animated Meta Data
-                        case "ao": // Animated Object
-                        case "aoc": // Animated Object Controller
-                        case "arl":
-                        case "arm": // Rooms
-                        case "atlas":
-                        case "cht": // ChestData
-                        case "clt":
-                        case "dct": // Decals
-                        case "ddt": // Doodads
-                        case "dlp":
-                        case "ecf":
-                        case "env": // Environment
-                        case "epk":
-                        case "et":
-                        case "ffx": // FFX Render
-                        case "gft":
-                        case "gt": // Ground Types
-                        case "idl":
-                        case "idt":
-                        case "mat": // Materials
-                        case "properties":
-                        case "sm": // Skin Mesh
-                        case "tgr":
-                        case "tgt":
-                        case "tsi":
-                        case "tst":
-                        case "txt":
-                        case "ui": // User Interface
-                        case "xml":
+                        case ".act":
+                        case ".ais":
+                        case ".amd": // Animated Meta Data
+                        case ".ao": // Animated Object
+                        case ".aoc": // Animated Object Controller
+                        case ".arl":
+                        case ".arm": // Rooms
+                        case ".atlas":
+                        case ".cht": // ChestData
+                        case ".clt":
+                        case ".dct": // Decals
+                        case ".ddt": // Doodads
+                        case ".dlp":
+                        case ".ecf":
+                        case ".env": // Environment
+                        case ".epk":
+                        case ".et":
+                        case ".ffx": // FFX Render
+                        case ".gft":
+                        case ".gt": // Ground Types
+                        case ".idl":
+                        case ".idt":
+                        case ".mat": // Materials
+                        case ".properties":
+                        case ".sm": // Skin Mesh
+                        case ".tgr":
+                        case ".tgt":
+                        case ".tsi":
+                        case ".tst":
+                        case ".txt":
+                        case ".ui": // User Interface
+                        case ".xml":
                             _DataFormat = DataFormats.Unicode;
                             break;
-                        case "csv":
-                        case "filter": // Item/loot filter
-                        case "fx": // Shader
-                        case "hlsl": // Shader
-                        case "mel": // Maya Embedded Language
-                        case "mtd":
-                        case "ot":
-                        case "otc":
-                        case "pet":
-                        case "red":
-                        case "rs": // Room Set
-                        case "slt":
-                        case "trl": // Trace log?
+                        case ".csv":
+                        case ".filter": // Item/loot filter
+                        case ".fx": // Shader
+                        case ".hlsl": // Shader
+                        case ".mel": // Maya Embedded Language
+                        case ".mtd":
+                        case ".ot":
+                        case ".otc":
+                        case ".pet":
+                        case ".red":
+                        case ".rs": // Room Set
+                        case ".slt":
+                        case ".trl": // Trace log?
                             _DataFormat = DataFormats.Ascii;
                             break;
-                        case "dat":
-                        case "dat64":
+                        case ".dat":
+                        case ".dat64":
                             _DataFormat = DataFormats.Dat;
                             break;
-                        case "dds":
+                        case ".dds":
                             _DataFormat = DataFormats.TextureDds;
                             break;
-                        case "jpg":
-                        case "png":
+                        case ".jpg":
+                        case ".png":
                             _DataFormat = DataFormats.Image;
                             break;
-                        case "ogg":
+                        case ".ogg":
                             _DataFormat = DataFormats.OGG;
                             break;
-                        case "bk2":
+                        case ".bk2":
                             _DataFormat = DataFormats.BK2;
                             break;
-                        case "bank":
+                        case ".bank":
                             _DataFormat = DataFormats.BANK;
                             break;
                         default:
